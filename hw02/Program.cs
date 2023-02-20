bf2b371ad7a6c82055a0157d654ce96ef83b3672
Natalyae <ngk.s@mail.ru>
2023-02-20 05:21:30 +0100

    Add code hw02

--- hw02/Program.cs
+++ hw02/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+int n = 3;
+int num3 = 0;
+if (number < 100){
+    Console.WriteLine($"У числа {number} нет третий цифры");
+} else {
+    for (int i = 1; i < n; i++){
+        num = num/10;
+    }
+
+    if(num < 10) num3 = num;
+    else num3 = num%10;
+    
+    Console.WriteLine($"У числа {number} третья цифра с начала {num3}");
+}
+
