d0f4f7d350b1a71ddbd88e656024b52296ca9489
Natalyae <ngk.s@mail.ru>
2023-02-20 05:41:20 +0100

    Add code hw03

--- hw03/Program.cs
+++ hw03/Program.cs
@@ -2,3 +2,13 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+
+Console.WriteLine ("Введите номер дня недели");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday < 1 || weekday > 7){
+    Console.WriteLine("Неверно введен номер");
+}else if(weekday == 6 || weekday == 7){
+    Console.WriteLine($"{weekday} - выходной день");
+}else{
+    Console.WriteLine($"{weekday} - будний день");
+}
