316264268aff8bb35552852cd7fb90027aad9674
Natalyae <ngk.s@mail.ru>
2023-02-20 04:31:56 +0100

    Add code hw01

--- hw01/Program.cs
+++ hw01/Program.cs
@@ -1,6 +1,14 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 //456 -> 5
 //782 -> 8
-//918 -> 
+//918 -> 1
 
-Console.WriteLine("D")
+Console.WriteLine("Ввкдите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000){
+    int number10 = number/10;
+    int TwoNumber = number10%10;
+    Console.WriteLine($"{number} вторая цифра {TwoNumber}");
+}else{
+    Console.WriteLine("Неверно введено число");
+}
